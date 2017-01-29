20a5bd5be690fb8f99485209cddda16f17aafd22
louismurat <lmurat@laposte.net>
2017-01-29 22:12:03 +0100

    final version

--- Simulation.py
+++ Simulation.py
@@ -3,7 +3,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import random
 
+random.seed(3456)
 
+
+# creation of the simulation of the thousand throws
 thousandsthrows = [random.randint(1, 6)+random.randint(1, 6) for _ in range(1000)]
 
 labels, values = zip(*sorted(Counter(thousandsthrows).items()))
@@ -13,15 +16,18 @@ plt.bar(indexes, values, width)
 plt.xticks(indexes + width * 0.5, labels)
 plt.show()
 
+# we create a millions of throws
 millions_throws = [random.randint(1, 6)+random.randint(1, 6) for _ in range(1000000)]
 print(Counter(millions_throws))
-
+# we sort them
 list_resultat = sorted(Counter(millions_throws).items())
 print(list_resultat)
+# we obtain the number of result of throws of dices
 list_nombre = []
 for i, j in list_resultat:
     list_nombre.append(int(j))
 
+# we multiply by a possible list of return amount
 list_retour = np.array([30, 15, 10, 7.5, 6, 4, 6, 7.5, 10, 15, 30])
 print(list_nombre)
 print(list_nombre*list_retour)
